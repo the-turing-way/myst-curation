390ec289a9cbffb77ee8d692cfe3983ba9f622a5
Jim Madge <jim@jmadge.com>
2025-06-10 15:51:34 +0100

    Add description option

--- curation/curation.mjs
+++ curation/curation.mjs
@@ -6,7 +6,7 @@ const curationDirective = {
     doc: "curation",
     arg: {
         type: String,
-        doc: "arg",
+        doc: "Curation title",
     },
     body: {
         type: String,
@@ -16,36 +16,56 @@ const curationDirective = {
     options: {
         depth: {
             type: Number,
-            doc: "heading depth"
+            doc: "Heading depth",
+        },
+        description: {
+            type: String,
+            doc: "Curation description",
         },
     },
     run(data, vfile, ctx) {
+        // Process options
+        const depth = data.options?.depth ?? 2;
+        const description = data.options?.description ?? null;
+        // let description = "hello";
+
         // Validate ToC
         let obj = yaml.load(data.body);
         let opts = { property: "test" }; // I don't know what this does 😱
         let toc = validateTOC(obj, opts) ?? "oh no!";
         console.log(toc);
-        // Process options
-        const depth = data.options?.depth ?? 2;
-        return [{
-            type: "block",
+
+        // Process ToC
+        toc = ctx.parseMyst(data.body);
+        console.log(toc);
+
+        let items = [];
+
+        // Add heading
+        items.push({
+            type: "heading",
+            depth: depth,
+            enumerated: false,
             children: [
-                {
-                    type: "heading",
-                    depth: depth,
-                    enumerated: false,
-                    children: [
-                        { type: "text", value: data.arg },
-                    ],
-                },
-                {
-                    type: "paragraph",
-                    children: [
-                        { type: "text", value: data.body },
-                    ],
-                },
-            ]
-        }];
+                { type: "text", value: data.arg },
+            ],
+        });
+
+        // Add desscription
+        if (description) {
+            items.push({
+                type: "paragraph",
+                children: [
+                    // { type: "text", value: description },
+                    ctx.parseMyst(description).children[0]
+                ],
+            })
+        };
+
+        // Add Toc
+        items.push(toc.children[0])
+
+        return [{type: "block", children: items}];
     },
 };
 
--- demo/index.md
+++ demo/index.md
@@ -4,8 +4,13 @@
 :context: project
 :::
 
-:::{curation} my favourites
-:depth: 2
+:::{curation} My Favourites
+---
+depth: 2
+description: >
+  My favourite things in this book.
+  Chosen by **me**.
+---
 - file: animals/cats.md
 - file: cinemas/prince_charles.md
 - file: places/lundy.md
