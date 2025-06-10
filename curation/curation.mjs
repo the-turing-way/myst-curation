e58e472b0d8a2bc179f25cb7c69a1dbb382e775b
Jim Madge <jim@jmadge.com>
2025-06-10 11:26:10 +0100

    Remove unused dependency

--- curation/curation.mjs
+++ curation/curation.mjs
@@ -1,6 +1,5 @@
 import { validateTOC } from 'myst-toc';
 import yaml from 'js-yaml';
-import { resolveArticlesFromTOC } from 'myst-cli';
 
 const curationDirective = {
     name: "curation",
@@ -21,7 +20,6 @@ const curationDirective = {
         let obj = yaml.load(data.body);
         console.log(obj);
         let opts = { property: "test" }; // I don't know what this does 😱
-        // toc = validateTOC( [{ file: "foo.md" }], { property: "test" } );
         toc = validateTOC(obj, opts) ?? "oh no!";
         console.log(toc);
         return [{
